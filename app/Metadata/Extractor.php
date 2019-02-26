386474dd12c486192eb118983f53dcf8164cd6a4
Hermann Käser <hermann.kaser@gmail.com>
2019-02-26 16:38:12 -0500

    WIP

--- app/Metadata/Extractor.php
+++ app/Metadata/Extractor.php
@@ -2,6 +2,8 @@
 
 namespace App\Metadata;
 
+use PHPExif\Reader\Reader;
+
 class Extractor
 {
 	/**
@@ -12,23 +14,23 @@ class Extractor
 	 */
 	public function extract(string $filename): array
 	{
-		$imageInfo = [];
-		$info = getimagesize($filename, $imageInfo);
+		$reader = Reader::factory(Reader::TYPE_NATIVE);
+		$exif = $reader->read($filename);
 
 		$metadata = [
-			'type'        => $info['mime'],
-			'width'       => $info[0],
-			'height'      => $info[1],
-			'title'       => '',
+			'type'        => $exif->getMimeType(),
+			'width'       => $exif->getWidth(),
+			'height'      => $exif->getHeight(),
+			'title'       => $exif->getTitle(),
 			'description' => '',
-			'orientation' => '',
-			'iso'         => '',
-			'aperture'    => '',
+			'orientation' => $exif->getOrientation(),
+			'iso'         => $exif->getIso(),
+			'aperture'    => $exif->getAperture(),
 			'make'        => '',
-			'model'       => '',
-			'shutter'     => '',
-			'focal'       => '',
-			'takestamp'   => null,
+			'model'       => $exif->getCamera(),
+			'shutter'     => $exif->getExposure(),
+			'focal'       => $exif->getFocalLength(),
+			'takestamp'   => $exif->getCreationDate(),
 			'lens'        => '',
 			'tags'        => '',
 			'position'    => '',
@@ -46,194 +48,45 @@ class Extractor
 			$metadata['size'] = round($size, 1).' KB';
 		}
 
-		// IPTC Metadata
-		// See https://www.iptc.org/std/IIM/4.2/specification/IIMV4.2.pdf for mapping
-		if (isset($imageInfo['APP13'])) {
-			$imageInfo = iptcparse($imageInfo['APP13']);
-			if (is_array($imageInfo)) {
-				// Title
-				if (!empty($imageInfo['2#105'][0])) {
-					$metadata['title'] = $imageInfo['2#105'][0];
-				}
-				else {
-					if (!empty($imageInfo['2#005'][0])) {
-						$metadata['title'] = $imageInfo['2#005'][0];
-					}
-				}
+		if (!empty($metadata['focal'])) {
+			$metadata['focal'] .= ' mm';
+		}
 
-				// Description
-				if (!empty($imageInfo['2#120'][0])) {
-					$metadata['description'] = $imageInfo['2#120'][0];
-				}
+		$rawExifData = $exif->getRawData();
 
-				// Tags
-				if (!empty($imageInfo['2#025'])) {
-					$metadata['tags'] = implode(',', $imageInfo['2#025']);
-				}
+		if (!empty($rawExifData['IFD0:Make'])) {
+			$metadata['make'] = trim($rawExifData['IFD0:Make']);
+		} elseif (!empty($rawExifData['Make'])) {
+			$metadata['make'] = trim($rawExifData['Make']);
+		}
 
-				// Position
-				$fields = array();
-				if (!empty($imageInfo['2#090'])) {
-					$fields[] = trim($imageInfo['2#090'][0]);
-				}
-				if (!empty($imageInfo['2#092'])) {
-					$fields[] = trim($imageInfo['2#092'][0]);
-				}
-				if (!empty($imageInfo['2#095'])) {
-					$fields[] = trim($imageInfo['2#095'][0]);
-				}
-				if (!empty($imageInfo['2#101'])) {
-					$fields[] = trim($imageInfo['2#101'][0]);
-				}
+		if (!empty($exif->getGps())) {
+			list($metadata['latitude'], $metadata['longitude']) = explode(',', $exif->getGps());
+		}
 
-				if (!empty($fields)) {
-					$metadata['position'] = implode(', ', $fields);
-				}
+		// isset check for the altitude ref because the value can be 0
+		if (!empty($rawExifData['GPS:GPSAltitude']) && isset($rawExifData['GPS:GPSAltitudeRef'])) {
+			$metadata['altitude'] =
+				$this->getGPSAltitude($rawExifData['GPS:GPSAltitude'], $rawExifData['GPS:GPSAltitudeRef']);
+		} elseif (!empty($rawExifData['GPSAltitude']) && isset($rawExifData['GPSAltitudeRef'])) {
+			$metadata['altitude'] =
+				$this->getGPSAltitude($rawExifData['GPSAltitude'], $rawExifData['GPSAltitudeRef']);
+		}
+
+		foreach ($rawExifData as $key => $value) {
+			if (strpos($key, 'LensModel') !== false) {
+				$metadata['lens'] = $value;
 			}
 		}
 
-		// Read EXIF
-		if ($info['mime'] == 'image/jpeg') {
-			$exif = @exif_read_data($filename, 'EXIF', false, false);
-		}
-		else {
-			$exif = false;
-		}
-
-		// EXIF Metadata
-		if ($exif !== false) {
-			// Orientation
-			if (isset($exif['Orientation'])) {
-				$metadata['orientation'] = $exif['Orientation'];
-			}
-			else {
-				if (isset($exif['IFD0']['Orientation'])) {
-					$metadata['orientation'] = $exif['IFD0']['Orientation'];
-				}
-			}
-
-			// ISO
-			if (!empty($exif['ISOSpeedRatings'])) {
-				$metadata['iso'] = $exif['ISOSpeedRatings'];
-			}
-
-			// Aperture
-			if (!empty($exif['COMPUTED']['ApertureFNumber'])) {
-				$metadata['aperture'] = $exif['COMPUTED']['ApertureFNumber'];
-			}
-
-			// Make
-			if (!empty($exif['Make'])) {
-				$metadata['make'] = trim($exif['Make']);
-			}
-
-			// Model
-			if (!empty($exif['Model'])) {
-				$metadata['model'] = trim($exif['Model']);
-			}
-
-			// Exposure
-			if (!empty($exif['ExposureTime'])) {
-				$metadata['shutter'] = $exif['ExposureTime'].' s';
-			}
-
-			// Focal Length
-			if (!empty($exif['FocalLength'])) {
-				if (strpos($exif['FocalLength'], '/') !== false) {
-					$temp = explode('/', $exif['FocalLength'], 2);
-					$temp = $temp[0] / $temp[1];
-					$temp = round($temp, 1);
-					$metadata['focal'] = $temp.' mm';
-				}
-				else {
-					$metadata['focal'] = $exif['FocalLength'].' mm';
-				}
-			}
-
-			// Takestamp
-			if (!empty($exif['DateTimeOriginal'])) {
-				if ($exif['DateTimeOriginal'] == '0000:00:00 00:00:00') {
-					$metadata['takestamp'] = null;
-				}
-				else {
-					if (strtotime($exif['DateTimeOriginal']) == 0) {
-						$metadata['takestamp'] = null;
-					}
-					else {
-						$metadata['takestamp'] = date("Y-m-d H:i:s", strtotime($exif['DateTimeOriginal']));
-					}
-				}
-			}
-
-			if (!empty($exif['LensInfo'])) {
-				$metadata['lens'] = trim($exif['LensInfo']);
-			}
-			// Lens field from Lightroom
-			if ($metadata['lens'] == '' && !empty($exif['UndefinedTag:0xA434'])) {
-				$metadata['lens'] = trim($exif['UndefinedTag:0xA434']);
-			}
-
-			// Deal with GPS coordinates
-			if (!empty($exif['GPSLatitude']) && !empty($exif['GPSLatitudeRef'])) {
-				$metadata['latitude'] = $this->getGPSCoordinate($exif['GPSLatitude'], $exif['GPSLatitudeRef']);
-			}
-			if (!empty($exif['GPSLongitude']) && !empty($exif['GPSLongitudeRef'])) {
-				$metadata['longitude'] = $this->getGPSCoordinate($exif['GPSLongitude'], $exif['GPSLongitudeRef']);
-			}
-			if (!empty($exif['GPSAltitude']) && !empty($exif['GPSAltitudeRef'])) {
-				$metadata['altitude'] = $this->getGPSAltitude($exif['GPSAltitude'], $exif['GPSAltitudeRef']);
-			}
+		if (empty($metadata['lens']) && !empty($rawExifData['LensInfo'])) {
+			$metadata['lens'] = trim($rawExifData['LensInfo']);
+		} elseif (empty($metadata['lens']) && !empty($rawExifData['UndefinedTag:0x0095'])) {
+			$metadata['lens'] = trim($rawExifData['UndefinedTag:0x0095']);
 		}
 
 		return $metadata;
 	}
-
-
-
-	/**
-	 * Returns the normalized coordinate from EXIF array
-	 *
-	 * @param array $coordinate
-	 * @param string $ref
-	 * @return float Normalized coordinate as float number (degrees)
-	 */
-	private function getGPSCoordinate(array $coordinate, string $ref): float
-	{
-		$degrees = count($coordinate) > 0 ? $this->formattedToFloatGPS($coordinate[0]) : 0;
-		$minutes = count($coordinate) > 1 ? $this->formattedToFloatGPS($coordinate[1]) : 0;
-		$seconds = count($coordinate) > 2 ? $this->formattedToFloatGPS($coordinate[2]) : 0;
-
-		$flip = ($ref == 'W' || $ref == 'S') ? -1 : 1;
-
-		return $flip * ($degrees + (float) $minutes / 60 + (float) $seconds / 3600);
-	}
-
-
-
-	/**
-	 * Converts a `rational64u` to a float (`29451/625 => 47.1216`)
-	 *
-	 * @param  string $rational
-	 * @return float
-	 */
-	private function formattedToFloatGPS(string $rational): float
-	{
-		$parts = explode('/', $rational, 2);
-
-		if (count($parts) <= 0) {
-			return 0.0;
-		}
-		if (count($parts) == 1) {
-			return (float) $parts[0];
-		}
-		// case part[1] is 0, div by 0 is forbidden.
-		if ($parts[1] == 0)
-			return (float) 0;
-
-		return (float) $parts[0] / $parts[1];
-	}
-
-
 
 	/**
 	 * Returns the altitude either above or below sea level
@@ -245,6 +98,6 @@ class Extractor
 	private function getGPSAltitude(string $altitude, string $ref): float
 	{
 		$flip = ($ref == '1') ? -1 : 1;
-		return $flip * $this->formattedToFloatGPS($altitude);
+		return $flip * (float) $altitude;
 	}
 }
